82ad7d87d670ffea8a04b964e678a18cf695c9c0
Kondi-ma <elongakondima@gmail.com>
2023-12-04 18:51:09 -0800

    correction3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
-    [print('{:d}'.format(j)) for j in reversed(my_list)]
+    for j in reversed(my_list):
+        print('{:d}'.format(j))
